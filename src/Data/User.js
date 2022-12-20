26c30c266c9ea25c229ff8b256113a91a662ee44
TheKsiZ <somethingksiz@gmail.com>
2022-12-20 16:56:43 +0300

    created class for user

---
+++ src/Data/User.js
@@ -0,0 +1,8 @@
+class User {
+    constructor(options){
+        this.userId = options.userId;
+        this.email = options.email;
+        this.firstName = options.firstName;
+        this.lastName = options.lastName;
+    }
+}
