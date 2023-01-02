4808f9d251fbecf31556db164941531867cf7674
TheKsiZ <somethingksiz@gmail.com>
2023-01-02 15:20:49 +0300

    added black theme for mui components

---
+++ src/muiComponents/MUIBlackTheme.jsx
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material"
+
+const MUIBlackTheme = createTheme({
+    palette:{
+        primary:{
+            main: '#000000',
+        },
+    }
+})
+
+export default MUIBlackTheme;
