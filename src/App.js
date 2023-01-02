d03e99e7bc1483e9b750f9d52f9ec90638eb39d1
TheKsiZ <somethingksiz@gmail.com>
2023-01-02 15:20:28 +0300

    created routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
-import Login from "./Login";
+import {Routes, Route, Link} from 'react-router-dom';
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Registration from "./pages/Registration";
+import Rooms from "./pages/Rooms";
+import NotFound from "./pages/NotFound";
+
 const App = () => {
   return(
-    <main>
-      <Login/>
-    </main>
+    <>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/registration" element={<Registration/>}/>        
+        <Route path="/rooms" element={<Rooms/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
+    </>
   );
 }
 
